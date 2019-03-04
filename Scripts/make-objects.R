e7bf48f2c663ae89988bb95620a55fe3b27871c8
Ole Kristian Saksberg <olekristian.saksberg@gmail.com>
2019-03-04 18:13:53 +0100

    Endret strategi og gikk fra Wide til Tidy struktur

--- Scripts/make-objects.R
+++ Scripts/make-objects.R
@@ -1,27 +1,51 @@
 library(dplyr)
 library(tidyr)
+library(ggplot2)
 
-fordeling_2015 <- read.csv("Data/Partifordeling2015.csv", sep = ";" )
+fordeling_2015 <- read.csv("Data/Partifordeling2015.csv", sep = ";", stringsAsFactors = FALSE )
 fordeling_2015 <- fordeling_2015 %>%
   filter(Kommunenavn %in% c("Spydeberg", "Askim", "HobÃ¸l", "Eidsberg", "TrÃ¸gstad")) %>%
-  select(Kommunenavn, Partinavn, Antall.stemmeberettigede, Antall.stemmer.totalt) %>%
-  mutate(Antall.avgitte.stemmer = sum(Antall.stemmer.totalt)) %>%
-  spread(Partinavn, Antall.stemmer.totalt) %>%
+  select(Kommunenavn, Partinavn, Oppslutning.prosentvis, Antall.stemmeberettigede, Antall.stemmer.totalt) %>%
   mutate(Year = 2015)
 
-Indre <- c("Indre_Ostfold", sum(fordeling_2015$Antall.stemmeberettigede, na.rm = TRUE), 
-           sum(fordeling_2015$Antall.avgitte.stemmer, na.rm = TRUE), sum(fordeling_2015$Arbeiderpartiet, na.rm = TRUE), 
-           sum(fordeling_2015$Blanke, na.rm = TRUE),
-           sum(fordeling_2015$Fremskrittspartiet, na.rm = TRUE),
-           sum(fordeling_2015$`HÃ¸yre`, na.rm = TRUE),
-           sum(fordeling_2015$`Kristelig Folkeparti`, na.rm = TRUE),
-           sum(fordeling_2015$`MiljÃ¸partiet De GrÃ¸nne`, na.rm = TRUE),
-           sum(fordeling_2015$Senterpartiet, na.rm = TRUE),
-           sum(fordeling_2015$`Sosialistisk Venstreparti`, na.rm = TRUE),
-           sum(fordeling_2015$Venstre, na.rm = TRUE),
-           2015)
-Indre <- as.data.frame(t(Indre))
-colnames(Indre) <- kolonner
-fordeling_2015 <- rbind(fordeling_2015, Indre)
+fordeling_2015$Oppslutning.prosentvis <- gsub(",", ".", fordeling_2015$Oppslutning.prosentvis)
 
-  
+fordeling_2015 <- mutate(fordeling_2015, Oppslutning.prosentvis = as.double(Oppslutning.prosentvis))
+
+Indre2015 <- fordeling_2015 %>%
+  group_by(Partinavn) %>%
+  summarize(Oppslutning.prosentvis = mean(Oppslutning.prosentvis),
+            Antall.stemmer.totalt = sum(Antall.stemmer.totalt, na.rm = TRUE),
+            sum(unique(Antall.stemmeberettigede))) %>%
+            mutate(Kommunenavn = "Indre_Ostfold", Year = 2015, Antall.stemmeberettigede = 33872)
+
+fordeling_2015 <- merge(fordeling_2015, Indre2015, all = TRUE)
+ 
+
+
+fordeling_2011 <- read.csv("Data/Partifordeling2011.csv", sep = ";", stringsAsFactors = FALSE )
+fordeling_2011 <- fordeling_2011 %>%
+  filter(Kommunenavn %in% c("Spydeberg", "Askim", "HobÃ¸l", "Eidsberg", "TrÃ¸gstad")) %>%
+  select(Kommunenavn, Partinavn, Oppslutning.prosentvis, Antall.stemmeberettigede, Antall.stemmer.totalt) %>%
+  mutate(Year = 2011)
+
+fordeling_2011$Oppslutning.prosentvis <- gsub(",", ".", fordeling_2011$Oppslutning.prosentvis)
+
+fordeling_2011 <- mutate(fordeling_2011, Oppslutning.prosentvis = as.double(Oppslutning.prosentvis))
+
+Indre2011 <- fordeling_2011 %>%
+  group_by(Partinavn) %>%
+  summarize(Oppslutning.prosentvis = mean(Oppslutning.prosentvis),
+            Antall.stemmer.totalt = sum(Antall.stemmer.totalt, na.rm = TRUE),
+            sum(unique(Antall.stemmeberettigede))) %>%
+  mutate(Kommunenavn = "Indre_Ostfold", Year = 2011, Antall.stemmeberettigede = 31919)
+
+fordeling_2011 <- merge(fordeling_2011, Indre2011, all = TRUE)
+
+kommune_valg_indreOstfold <- merge(fordeling_2011, fordeling_2015, all = TRUE)
+
+stemmeandel <- kommune_valg_indreOstfold %>%
+  group_by(Kommunenavn, Year) %>%
+  summarize(Andel_som_stemte = (sum(Antall.stemmer.totalt) / sum(unique(Antall.stemmeberettigede)))*100,
+            Antall_stemmer = sum(Antall.stemmer.totalt),
+            Antall.stemmeberettigede = sum(unique(Antall.stemmeberettigede)))
