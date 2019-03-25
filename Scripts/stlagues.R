3ebaa3122f9a4c0bafc9dc19be76b0d7569d3532
Ole Kristian Saksberg <olekristian.saksberg@gmail.com>
2019-03-25 08:22:17 +0100

    St. Lagues lagt til

--- Scripts/stlagues.R
+++ Scripts/stlagues.R
@@ -1,25 +1,25 @@
 library(purrr)
 
 MandaterIndre <- kommune_valg_indreOstfold %>%
-  filter(Kommunenavn == "Indre", År == 2015, Partinavn != "Blanke") %>%
+  filter(Kommunenavn == "Askim", År == 2015, Partinavn != "Blanke") %>%
   select(Partikode, Antall.stemmer.totalt) %>%
   mutate(Mandater = 0)
 
-head(MandaterIndre)
+print(MandaterIndre)
 
-j = c()
-ind <- 0
-for(i in 1:8) {
-  k <- MandaterIndre[i,2]
-  j <- append(j, k/1.4 )
-}
-ind <- j[] == max(j)
-MandaterIndre[ind,3] <- MandaterIndre[ind,3] +1
-print(ind)
-print(j)
+# j = c()
+# ind <- 0
+# for(i in 1:8) {
+#   k <- MandaterIndre[i,2]
+#   j <- append(j, k/1.4 )
+# }
+# ind <- j[] == max(j)
+# MandaterIndre[ind,3] <- MandaterIndre[ind,3] +1
+# print(ind)
+# print(j)
 
 m <- 0
-while( m <= 48){
+while( m <= 34){
   j = c()
   ind <- 0
   
@@ -36,5 +36,6 @@ while( m <= 48){
   print(j)
 }
 
+MandaterIndre <- MandaterIndre %>% arrange(desc(Mandater))
 
-
+kable(MandaterIndre)
